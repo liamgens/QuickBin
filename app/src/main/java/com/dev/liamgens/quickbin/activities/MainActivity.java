19d278e6472344a918a2ac6c76fdbd0a622c06c9
leshan <ianleshan71@gmail.com>
2016-09-17 14:42:08 -0400

    Upload picture

--- app/src/main/java/com/dev/liamgens/quickbin/activities/MainActivity.java
+++ app/src/main/java/com/dev/liamgens/quickbin/activities/MainActivity.java
@@ -1,15 +1,50 @@
 package com.dev.liamgens.quickbin.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.dev.liamgens.quickbin.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int RC_SIGN_IN = 120;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+        } else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setProviders(
+                                    AuthUI.GOOGLE_PROVIDER)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // user is signed in!
+                startActivity(new Intent(this, BinList.class));
+                finish();
+            } else {
+                // user is not signed in. Maybe just wait for the user to press
+                // "sign in" again, or show a message
+            }
+        }
     }
 }
