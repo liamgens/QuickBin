4aae2b2d2c931bf7f76ff5cb238055b19d212619
Liam Gensel <liamgens@gmail.com>
2016-09-17 00:34:36 -0400

    I hope this breaks

--- app/src/main/java/com/dev/liamgens/quickbin/activities/ProfilePage.java
+++ app/src/main/java/com/dev/liamgens/quickbin/activities/ProfilePage.java
@@ -7,6 +7,8 @@ import android.widget.ImageView;
 import com.dev.liamgens.quickbin.R;
 import com.squareup.picasso.Picasso;
 
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
+
 public class ProfilePage extends AppCompatActivity {
     private ImageView _profilePicture;
     @Override
@@ -14,7 +16,7 @@ public class ProfilePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
         _profilePicture = (ImageView) findViewById(R.id.profile_picture);
-        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(_profilePicture);
+        Picasso.with(this).load("https://lh3.googleusercontent.com/Xb_HzSiWAsYDy-9uB03fVlFrAnUx1SRmD2DwB2JAvBBANBvypC03HR8Mnf7RNSg_iu8C8vl3SA=w5760-h3600-rw-no").transform(new CropCircleTransformation()).into(_profilePicture);
 
     }
 }
