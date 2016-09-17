19ec18700fac97e5b751d87031c0fa1421e09041
leshan <ianleshan71@gmail.com>
2016-09-16 22:35:44 -0400

    Added activities

--- app/src/main/java/com/dev/liamgens/quickbin/activities/Profile.java
+++ app/src/main/java/com/dev/liamgens/quickbin/activities/Profile.java
@@ -1,13 +1,15 @@
-package com.dev.liamgens.quickbin;
+package com.dev.liamgens.quickbin.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.dev.liamgens.quickbin.R;
+
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
     }
 }
