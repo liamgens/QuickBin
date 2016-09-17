1ef99ae2817718aa784d83235b8bfd188740d5e0
Liam Gensel <liamgens@gmail.com>
2016-09-17 00:09:36 -0400

    text sizes

--- app/src/main/java/com/dev/liamgens/quickbin/activities/ProfilePage.java
+++ app/src/main/java/com/dev/liamgens/quickbin/activities/ProfilePage.java
@@ -2,14 +2,19 @@ package com.dev.liamgens.quickbin.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.dev.liamgens.quickbin.R;
+import com.squareup.picasso.Picasso;
 
 public class ProfilePage extends AppCompatActivity {
-
+    private ImageView _profilePicture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+        _profilePicture = (ImageView) findViewById(R.id.profile_picture);
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(_profilePicture);
+
     }
 }
